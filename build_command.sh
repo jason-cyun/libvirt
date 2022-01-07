44457e4da19521f10e40a5a419aaa8898583f1dd
luozhangqing5 <luozhangqing5@jd.com>
2022-01-07 17:45:54 +0800

    enable dtrace

--- build_command.sh
+++ build_command.sh
@@ -1,3 +1,3 @@
 #! /usr/bin/env bash
-CFLAGS="-O0 -g" ./autogen.sh --system --with-bash-completion --with-openvz=no --with-uml=no --with-vmware=no --with-vbox=no --with-esx=no --with-dtrace=no --enable-debug=yes
+CFLAGS="-O0 -g" ./autogen.sh --system --with-bash-completion --with-openvz=no --with-uml=no --with-vmware=no --with-vbox=no --with-esx=no --with-dtrace=yes --enable-debug=yes
 
