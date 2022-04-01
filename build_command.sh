74c9c4ebc98a9c69231f39f4949eda82bc43e73e
luozhangqing5 <luozhangqing5@jd.com>
2022-04-01 08:22:24 +0800

    build clang to build

--- build_command.sh
+++ build_command.sh
@@ -1,3 +1,3 @@
 #! /usr/bin/env bash
-CFLAGS="-O0 -g" ./autogen.sh --system --with-bash-completion --with-openvz=no --with-uml=no --with-vmware=no --with-vbox=no --with-esx=no --with-dtrace=yes --enable-debug=yes
+CC=clang  CFLAGS="-O0 -g" ./autogen.sh --system --with-bash-completion --with-openvz=no --with-uml=no --with-vmware=no --with-vbox=no --with-esx=no --with-dtrace=yes --enable-debug=yes
 
