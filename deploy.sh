637130a35c4d9669ed379f8e373aa4602042d867
Jason Luo <luozhangqing5@jd.com>
2022-05-10 07:28:54 +0800

    add build depes

--- deploy.sh
+++ deploy.sh
@@ -3,22 +3,26 @@ if [ "$#" -eq 1 ] && [ "$1" = "-r" ]; then
   make clean
   clangdinit intercept-build
 else
-  make CC=clang  -j8
+  make CC=clang -j $(nproc)
 fi
 
-make install
+if [ $? -eq 0 ]; then
+  make install
 
-echo "log_level = 1" >> /etc/libvirt/libvirtd.conf
-echo 'log_outputs="1:file:/var/log/libvirt/libvirtd.log"' >> /etc/libvirt/libvirtd.conf
-echo 'keepalive_interval=60' >> /etc/libvirt/libvirtd.conf
-echo 'admin_keepalive_interval=60' >> /etc/libvirt/libvirtd.conf
+  echo "log_level = 1" >> /etc/libvirt/libvirtd.conf
+  echo 'log_outputs="1:file:/var/log/libvirt/libvirtd.log"' >> /etc/libvirt/libvirtd.conf
+  echo 'keepalive_interval=60' >> /etc/libvirt/libvirtd.conf
+  echo 'admin_keepalive_interval=60' >> /etc/libvirt/libvirtd.conf
 
-echo 'listen_tls = 0' >> /etc/libvirt/libvirtd.conf
-echo 'listen_tcp = 1' >> /etc/libvirt/libvirtd.conf
-echo 'auth_tcp = "none"' >> /etc/libvirt/libvirtd.conf
+  echo 'listen_tls = 0' >> /etc/libvirt/libvirtd.conf
+  echo 'listen_tcp = 1' >> /etc/libvirt/libvirtd.conf
+  echo 'auth_tcp = "none"' >> /etc/libvirt/libvirtd.conf
 
-echo 'LIBVIRTD_ARGS="--listen"' >> /etc/sysconfig/libvirtd
+  echo 'LIBVIRTD_ARGS="--listen"' >> /etc/sysconfig/libvirtd
 
-#echo 'uri_default = "qemu+tcp://127.0.0.1:16509/system"' >>/etc/libvirt/libvirt.conf
-systemctl daemon-reload
-service libvirtd restart
+  #echo 'uri_default = "qemu+tcp://127.0.0.1:16509/system"' >>/etc/libvirt/libvirt.conf
+  systemctl daemon-reload
+  service libvirtd restart
+else
+  echo "Failed to build"
+fi
